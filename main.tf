4d52489132d75dd3120ccdbf1b2e187faf0818fa
hidebon0630 <hyagi0630@icloud.com>
2021-10-13 17:24:02 +0900

    3.1 変数

--- main.tf
+++ main.tf
@@ -1,10 +1,8 @@
+variable "example_instance_type" {
+  default = "t2.micro"
+}
+
 resource "aws_instance" "example" {
   ami = "ami-0c3fd0f5d33134a76"
-  instance_type = "t2.micro"
-
-  user_data = <<EOF
-    #!bin/bash
-    yum install -y httpd
-    systemctl start httpd.service
-    EOF
+  instance_type = var.example_instance_type
 }
