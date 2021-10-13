f2292640bc273ec5ee0594f69c457e4a21033f70
hidebon0630 <hyagi0630@icloud.com>
2021-10-14 02:04:02 +0900

    7.4 セキュリティグループルール（アウトバウント）の定義

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-resource "aws_security_group_rule" "ingress_example" {
-  type = "ingress"
-  from_port = "80"
-  to_port = "80"
-  protocol = "tcp"
+resource "aws_security_group_rule" "egress_example" {
+  type = "egress"
+  from_port = "0"
+  to_port = "0"
+  protocol = "-1"
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.example.id
 }
