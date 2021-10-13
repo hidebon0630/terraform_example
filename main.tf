5628793c07f92adaa7d07b94e5cee3601faba978
hidebon0630 <hyagi0630@icloud.com>
2021-10-14 00:04:25 +0900

    5.2.2 IAMロール

--- main.tf
+++ main.tf
@@ -1,10 +1,4 @@
-data "aws_iam_policy_document" "ec2_assume_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
+resource "aws_iam_role" "example" {
+  name = "example"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
 }
