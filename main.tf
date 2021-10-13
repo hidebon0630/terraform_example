2aaaa28537e3be788c3106c61ff925a377e420d4
hidebon0630 <hyagi0630@icloud.com>
2021-10-14 00:10:37 +0900

    5.2.3 IAMポリシーのアタッチ

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role" "example" {
-  name = "example"
-  assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
+resource "aws_iam_role_policy_attachment" "example" {
+  role = aws_iam_role.example.name
+  policy_arn = aws_iam_policy.example.arn
 }
