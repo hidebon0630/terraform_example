7653dd50f39242be43830a76a25758a1b3f36d4d
hidebon0630 <hyagi0630@icloud.com>
2021-10-14 01:09:05 +0900

    7.2.1 プライベートルートテーブルと関連付けの定義

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
-resource "aws_subnet" "private" {
+resource "aws_route_table" "private" {
   vpc_id = aws_vpc.example.id
-  cidr_block = "10.0.64.0/24"
-  availability_zone = "ap-northeast-1a"
-  map_public_ip_on_launch = false
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
 }
