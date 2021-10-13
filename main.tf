2fe36cc53f7abe1b9751648bb17324013f0f7f6a
hidebon0630 <hyagi0630@icloud.com>
2021-10-14 00:02:19 +0900

    5.2.1 信頼ポリシー

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
-data "aws_iam_policy_document" "example" {
-  name = "example"
-  policy = data.aws_iam_policy_document.allow_describe_regions.json
+data "aws_iam_policy_document" "ec2_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
 }
