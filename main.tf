abb9de1e1a55c3a899b7a2a1a6e78625ea5a6ff4
hidebon0630 <hyagi0630@icloud.com>
2021-10-14 00:53:04 +0900

    7.1.3 インターネットゲートウェイ

--- main.tf
+++ main.tf
@@ -1,6 +1,3 @@
-resource "aws_subnet" "public" {
+resource "aws_internet_gateway" "example" {
   vpc_id = aws_vpc.example.id
-  cidr_block = "10.0.0.0/24"
-  map_public_ip_on_launch = true
-  availability_zone = "ap-northeast-1a"
 }
