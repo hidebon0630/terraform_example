58ec52c5ec9f2aaefea6698e9c91fc8eda498814
hidebon0630 <hyagi0630@icloud.com>
2021-10-14 00:40:42 +0900

    6.3.2 バケットポリシー

--- main.tf
+++ main.tf
@@ -1,11 +1,17 @@
-resource "aws_s3_bucket" "alb_log" {
-  bucket = "alb-log-pragmatic-terraform"
+resource "aws_s3_bucket_policy" "alb_log" {
+  bucket = aws_s3_bucket.alb_log.id
+  policy = data.aws_iam_policy_document.alb_log.json
+}
 
-  lifecycle_rule {
-    enabled = true
+data "aws_iam_policy_document" "alb_log" {
+  statement {
+    effect = "Allow"
+    actions = ["s3:PutObject"]
+    resources = ["arn:aws:s3:::${aws_s3_bucket.alb_log.id}/*"]
 
-    expiration {
-      days = "180"
+    principals {
+      type = "AWS"
+      identifiers = ["582318560864"]
     }
   }
 }
