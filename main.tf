94daf81acc7f2a3fd2e1878f1a4d1a588614011e
hidebon0630 <hyagi0630@icloud.com>
2021-10-14 00:33:22 +0900

    6.2 パブリックバケット

--- main.tf
+++ main.tf
@@ -1,7 +1,11 @@
-resource "aws_s3_bucket_public_access_block" "private" {
-  bucket = aws_s3_bucket.private.id
-  block_public_acls = true
-  block_public_policy = true
-  ignore_public_acls = true
-  restrict_public_buckets = true
+resource "aws_s3_bucket" "public" {
+  bucket = "public-pragmatic-terraform"
+  acl = "public-read"
+
+  cors_rule {
+    allowed_origins = ["https://example.com"]
+    allowed_methods = ["GET"]
+    allowed_headers = ["*"]
+    max_age_seconds = 3000
+  }
 }
