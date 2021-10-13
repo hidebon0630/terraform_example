2b29c8ed3500c074a88662233454e267a46a1a88
hidebon0630 <hyagi0630@icloud.com>
2021-10-14 01:39:09 +0900

    7.2.2 EIPの定義

--- main.tf
+++ main.tf
@@ -1,8 +1,4 @@
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.example.id
-}
-
-resource "aws_route_table_association" "private" {
-  subnet_id = aws_subnet.private.id
-  route_table_id = aws_route_table.private.id
+resource "aws_eip" "nat_gateway" {
+  vpc = true
+  depends_on = [aws_internet_gateway.example]
 }
