23d85f433f4a9c89019a32ef7a1f79c16e701aaf
hidebon0630 <hyagi0630@icloud.com>
2021-10-15 08:28:34 +0900

    9.3.1 ECSクラスタの定義

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-resource "aws_lb_listener_rule" "example" {
-  listener_arn = aws_lb_listener.https.arn
-  priority = 100
-
-  action {
-    type = "forward"
-    target_group_arn = aws_lb_target_group.example.arn
-  }
-
-  condition {
-    field = "path-pattern"
-    values = ["/*"]
-  }
+resource "aws_ecs_cluster" "example" {
+  name = "example"
 }
