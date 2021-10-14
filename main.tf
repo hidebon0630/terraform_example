7ed8719bace921f82eddaa3d031b079decaa1dab
hidebon0630 <hyagi0630@icloud.com>
2021-10-14 15:23:27 +0900

    8.5.2 HTTPからHTTPSにリダイレクトするリスナーの定義

--- main.tf
+++ main.tf
@@ -1,17 +1,15 @@
-resource "aws_lb_listener" "https" {
+resource "aws_lb_listener" "redirect_http_to_https" {
   load_balancer_arn = aws_lb.example.arn
-  port = "443"
-  protocol = "HTTPS"
-  certificate_arn = aws_acm_certificate.example.arn
-  ssl_policy = "ELBSecurityPolicy-2016-08"
+  port = "8080"
+  protocol = "HTTP"
 
   default_action {
-    type = "fixed-response"
+    type = "redirect"
 
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "これは「HTTPS」です"
-      status_code = "200"
+    redirect {
+      port = "443"
+      protocol = "HTTPS"
+      status_code = "HTTP_301"
     }
   }
 }
