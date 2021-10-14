f60e439d9d76b87226a8776b340d4b73004ba3e7
hidebon0630 <hyagi0630@icloud.com>
2021-10-14 15:04:59 +0900

    8.3.2 ホストゾーンの参照

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.example.arn
-  port = "80"
-  protocol = "HTTP"
-
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "これは「HTTP」です"
-      status_code = "200"
-    }
-  }
+data "aws_route53_zone" "example" {
+  name = "example.com"
 }
