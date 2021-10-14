f1733a22a63469e516223ea728132cedde5d6c01
hidebon0630 <hyagi0630@icloud.com>
2021-10-14 15:15:25 +0900

    8.4.1 SSL証明書の定義

--- main.tf
+++ main.tf
@@ -1,15 +1,9 @@
-resource "aws_route53_record" "example" {
-  zone_id = data.aws_route53_zone.example.zone_id
-  name = data.aws_route53_zone.example.name
-  type = "A"
+resource "aws_acm_certificate" "example" {
+  domain_name =  aws_route53_record.example.name
+  subject_alternative_names = []
+  validation_method = "DNS"
 
-  alias {
-    name = aws_lb.example.dns_name
-    zone_id = aws_lb.example.zone_id
-    evaluate_target_health = true
+  lifecycle {
+    create_before_destroy = true
   }
-}
-
-output "domain_name" {
-  value = aws_route53_record.example.name
 }
