6a220f357fe7276874738875f264a0ae2cc303b8
hidebon0630 <hyagi0630@icloud.com>
2021-10-15 12:35:31 +0900

    9.4.2 ECSタスク実行IAMロールのポリシードキュメントの定義

--- ecs.tf
+++ ecs.tf
@@ -53,3 +53,13 @@ resource "aws_cloudwatch_log_group" "for_ecs" {
   name = "./ecs/example"
   retention_in_days = 180
 }
+
+data "aws_iam_policy_document" "ecs_task_execution" {
+  source_json = data.aws_iam_policy.ecs_task_execution_role_policy.policy
+
+  statement {
+    effect = "Allow"
+    actions = ["ssm:GetParameters", "kms:Decrypt"]
+    resources = ["*"]
+  }
+}
