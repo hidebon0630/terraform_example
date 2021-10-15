e1fee34111225b498d9c20fb2cf46a065817e817
hidebon0630 <hyagi0630@icloud.com>
2021-10-15 19:40:30 +0900

    14.4.7 GitHub Webhookの定義

--- codepipeline.tf
+++ codepipeline.tf
@@ -127,3 +127,16 @@ resource "aws_codepipeline_webhook" "example" {
 provider "github" {
   organization = "hidebon0630"
 }
+
+resource "github_repository_webhook" "example" {
+  repository = "terraform_example"
+
+  configuration {
+    url = aws_codepipeline_webhook.example.url
+    secret = "VeryRandomStringMoreThan20Byte!"
+    content_type = "json"
+    insecure_ssl = false
+  }
+
+  events = ["push"]
+}
