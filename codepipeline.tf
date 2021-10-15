6bda290ac879e8a31fdc42d9209ed967c50962e1
hidebon0630 <hyagi0630@icloud.com>
2021-10-15 18:46:16 +0900

    14.4.1 CodePipelineサービスロールの定義

--- codepipeline.tf
+++ codepipeline.tf
@@ -21,3 +21,10 @@ data "aws_iam_policy_document" "codepipeline" {
     ]
   }
 }
+
+module "codepipeline_role" {
+  source = "./iam_role"
+  name = "codepipeline"
+  identifier = "codepipeline.amazonaws.com"
+  policy = data.aws_iam_policy_document.codepipeline.json
+}
