2f4f494d7c34a01c7d02c1d180d4aa4b051abed9
hidebon0630 <hyagi0630@icloud.com>
2021-10-15 12:37:47 +0900

    修正

--- ecs.tf
+++ ecs.tf
@@ -54,12 +54,16 @@ resource "aws_cloudwatch_log_group" "for_ecs" {
   retention_in_days = 180
 }
 
+data "aws_iam_policy" "ecs_task_execution_role_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 data "aws_iam_policy_document" "ecs_task_execution" {
   source_json = data.aws_iam_policy.ecs_task_execution_role_policy.policy
 
   statement {
-    effect = "Allow"
-    actions = ["ssm:GetParameters", "kms:Decrypt"]
+    effect    = "Allow"
+    actions   = ["ssm:GetParameters", "kms:Decrypt"]
     resources = ["*"]
   }
 }
