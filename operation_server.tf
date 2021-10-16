8c5c3a31ef118d93e609cd4931d6b38196d8ed23
hidebon0630 <hyagi0630@icloud.com>
2021-10-16 13:26:36 +0900

    15.2.3 Session Manager用SSM Documentの定義

--- operation_server.tf
+++ operation_server.tf
@@ -65,3 +65,21 @@ resource "aws_cloudwatch_log_group" "operation" {
   name = "/operation"
   retention_in_days = 180
 }
+
+resource "aws_ssm_document" "session_manager_run_shell" {
+  name = "SSM-SEssionManagerRunShell"
+  document_type = "Session"
+  document_format = "JSON"
+
+  content = <<EOF
+  {
+    "schemaVersion": "1.0",
+    "description": "Document to hold regional settings for Session Manager",
+    "sessionType": "Standard_Stream",
+    "inputs": {
+      "s3BucketName: "${aws_s3_bucket.operation.id}",
+      "cloudWatchLogGroupName": "${aws_cloudwatch_log_group.operation.name}
+    }
+  }
+  EOF
+}
