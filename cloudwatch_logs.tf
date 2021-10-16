6d819a87ce3f8ba9564ec6b91d7afad77f705c8b
hidebon0630 <hyagi0630@icloud.com>
2021-10-16 13:42:02 +0900

    16.3.2 Kinesis Data Firehose IAMロールのポリシードキュメントの定義

--- cloudwatch_logs.tf
+++ cloudwatch_logs.tf
@@ -9,3 +9,23 @@ resource "aws_s3_bucket" "cloudwatch_logs" {
     }
   }
 }
+
+data "aws_iam_policy_document" "kinesis_data_firehose" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "s3:AbortMultipartUpload",
+      "s3:GetBucketLocation",
+      "s3:GetObject",
+      "s3:ListBucket",
+      "s3:ListBucketMultipartUploads",
+      "s3:PutObject",
+    ]
+
+    resources = [
+      "arn:aws:s3:::${aws_s3_bucket.cloudwatch_logs.id}",
+      "arn:aws:s3:::${aws_s3_bucket.cloudwatch_logs.id}/*",
+    ]
+  }
+}
