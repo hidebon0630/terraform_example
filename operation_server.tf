69e8306e6f6f4aca65e42c0f23bfd4d5753403c0
hidebon0630 <hyagi0630@icloud.com>
2021-10-16 13:19:16 +0900

    15.2.2 オペレーションサーバー用EC2インスタンスの定義

--- operation_server.tf
+++ operation_server.tf
@@ -36,3 +36,15 @@ resource "aws_iam_instance_profile" "ec2_for_ssm" {
   name = "ec2-for-ssm"
   role = module.ec2_for_ssm_role.iam_role_name
 }
+
+resource "aws_instance" "example_for_operation" {
+  ami = "ami-0c3fd0f5d33134a76"
+  instance_type = "t2.micro"
+  iam_instance_profile = aws_iam_instance_profile.ec2_for_ssm.name
+  subnet_id = aws_subnet.private_0.id
+  user_data = file("./user_data.sh")
+}
+
+output "operation_instance_id" {
+  value = aws_instance.example_for_operation.id
+}
