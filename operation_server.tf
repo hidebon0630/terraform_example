d6a1316a16b2794579f6e66eb1815d342c2cc575
hidebon0630 <hyagi0630@icloud.com>
2021-10-16 13:13:17 +0900

    15.2.1 オペレーションサーバー用IAMロールの定義

--- operation_server.tf
+++ operation_server.tf
@@ -24,3 +24,10 @@ data "aws_iam_policy_document" "ec2_for_ssm" {
 data "aws_iam_policy" "ec2_for_ssm" {
   arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
+
+module "ec2_for_ssm_role" {
+  source = "./iam_role"
+  name = "ec2-for-ssm"
+  identifier = "ec2.amazonaws.com"
+  policy = data.aws_iam_policy_document.ec2_for_ssm.json
+}
